a49527f3ed9cc861bd4901e8e8bc335881aa34f1
Pahlevi Fikri Auliya <pahlevi_f@worksap.co.jp>
2014-11-12 12:10:39 +0800

    Enable basic auth

---
+++ src/jp/co/worksap/roster/controller/UserSessionController.java
@@ -0,0 +1,41 @@
+package jp.co.worksap.roster.controller;
+
+import java.io.Serializable;
+
+import javax.ejb.Stateless;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+@Stateless
+@Named
+public class UserSessionController implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	public String logout() {
+		System.out.println("Log out");
+		FacesContext context = FacesContext.getCurrentInstance();
+		HttpServletRequest request = (HttpServletRequest) context
+				.getExternalContext().getRequest();
+		try {
+			request.logout();
+		} catch (ServletException e) {
+			context.addMessage(null, new FacesMessage("Logout failed."));
+		}
+
+		return "/index.jsf";
+	}
+
+	public boolean isUserLoggedIn() {
+	    String user = this.getUserName();
+	    boolean result = !((user == null)|| user.isEmpty());
+	    return result;
+	}
+
+	public String getUserName() {
+	    String user = FacesContext.getCurrentInstance().getExternalContext().getRemoteUser();
+	    return user;
+	}
+}
