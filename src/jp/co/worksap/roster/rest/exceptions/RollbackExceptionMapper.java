e96db9dc247fa413fc43d69cc61e6103b7d20ec6
Pahlevi Fikri Auliya <pahlevi_f@worksap.co.jp>
2014-11-19 17:35:56 +0800

    Return constraint violation exception even during update

--- src/jp/co/worksap/roster/rest/exceptions/RollbackExceptionMapper.java
+++ src/jp/co/worksap/roster/rest/exceptions/RollbackExceptionMapper.java
@@ -3,6 +3,8 @@ package jp.co.worksap.roster.rest.exceptions;
 import java.sql.SQLException;
 
 import javax.transaction.RollbackException;
+import javax.validation.ConstraintViolationException;
+import javax.validation.ValidationException;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
@@ -21,6 +23,10 @@ public class RollbackExceptionMapper implements
     public Response toResponse(RollbackException ex) {
     	if (ex.getCause() != null && ex.getCause() instanceof DatabaseException && ex.getCause().getCause() != null && ex.getCause().getCause() instanceof SQLException) {
     		return Response.status(Status.INTERNAL_SERVER_ERROR).entity((new Gson()).toJson(new String[]{"The id is already used"})).type(MediaType.APPLICATION_JSON).build();
+    	}
+
+    	if (ex.getCause() != null && ex.getCause() instanceof ConstraintViolationException) {
+    		return (new ValidationExceptionMapper()).toResponse((ConstraintViolationException)ex.getCause());
     	}
 		return Response.status(Status.INTERNAL_SERVER_ERROR).entity((new Gson()).toJson(new String[]{"An error happen while saving your data"})).type(MediaType.APPLICATION_JSON).build();
     }
