6830f9bb413b972c5048eb334245e7fd8b90cf65
Pahlevi Fikri Auliya <pahlevi_f@worksap.co.jp>
2015-01-21 17:32:33 +0800

    Display map in reservation detail

--- WebContent/resources/js/controllers/reservations/reservation-detail.js
+++ WebContent/resources/js/controllers/reservations/reservation-detail.js
@@ -1,4 +1,4 @@
-var ReservationDetailController = function($scope, $state, $stateParams, $filter, $timeout, Reservations, Branches, TimezoneConverter, ngTableParams, ngProgress) {
+var ReservationDetailController = function($scope, $state, $stateParams, $filter, $timeout, Reservations, Branches, TimezoneConverter, ngTableParams, ngProgress, Inventories) {
 	console.log($stateParams.branchId);
 	console.log($stateParams.groupId);
 	$scope.reservations = [];
@@ -24,6 +24,9 @@ var ReservationDetailController = function($scope, $state, $stateParams, $filter
 
 				$scope.totalOverdueFee = _.reduce(d, function(memo, dd) { return memo + dd.overdueFee; }, 0);
 				$scope.reservations = d;
+
+				$scope.refreshMap();
+
 				$scope.endProgress();
 			}, function(d, h) {
 				$scope.endProgress();
@@ -111,6 +114,51 @@ var ReservationDetailController = function($scope, $state, $stateParams, $filter
 		});
 	};
 
+	$scope.refreshMap = function() {
+		$scope.$parent.branchResolved.promise.then(function() {
+			var mapOptions = {
+		      center: { lat: $scope.selectedBranch.latitude, lng: $scope.selectedBranch.longitude},
+		      zoom: 2
+		    };
+		    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+		    var oms = new OverlappingMarkerSpiderfier(map);
+		    var iw = new google.maps.InfoWindow();
+		    oms.addListener('click', function(marker, event) {
+		      iw.setContent(marker.desc);
+		      iw.open(map, marker);
+		    });
+
+		    oms.addListener('spiderfy', function(markers) {
+			  iw.close();
+			});
+
+			$scope.inventoryDetailLink = function(branchId, inventoryId, entity) {
+				return baseUrl + basePath + "/inventories/index.jsf#/" + branchId + "/inventories/" + inventoryId + "/detail?entity=" + entity;
+			};
+
+		    $scope.startProgress();
+
+		    console.log($scope.reservations);
+		    var carReservation = _.find($scope.reservations, function(d) { return d.inventory.type == 'car'});
+
+		    Inventories.get({branchId: $stateParams.branchId, id: carReservation.inventory.id},
+				function(car, h){
+					var marker = new google.maps.Marker({
+					      position: new google.maps.LatLng(car.latitude, car.longitude),
+					      map: map,
+					      title: car.name,
+					      desc: "<div style='width:200px; height:100px'><b><a target='_blank' href=" + $scope.inventoryDetailLink($stateParams.branchId, car.id, "car") + ">" + car.name + "</a></b>" + "<hr/><b>Location: (" + car.latitude + ", " + car.longitude + ")</b></div>"
+					});
+					oms.addMarker(marker);
+					$scope.endProgress();
+				},
+				function(d, h) {
+					$scope.endProgress();
+				}
+			);
+		});
+	};
+
 	$scope.parentUrl = baseUrl + basePath;
 	$scope.setSelectedBranch($stateParams.branchId);
 	$scope.refreshReservationDetail();
@@ -118,4 +166,4 @@ var ReservationDetailController = function($scope, $state, $stateParams, $filter
 
 angular.module('adminReservationManagementApp').controller('ReservationDetailController',
 		['$scope', '$state', '$stateParams', '$filter',  '$timeout',
-		 'Reservations', 'Branches', 'TimezoneConverter', 'ngTableParams', 'ngProgress', ReservationDetailController]);
+		 'Reservations', 'Branches', 'TimezoneConverter', 'ngTableParams', 'ngProgress', 'Inventories', ReservationDetailController]);
